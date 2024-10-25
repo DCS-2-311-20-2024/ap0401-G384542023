5c49b7816628d688f927005cab92f95fe765471d
G384542023 <238454@st.takushoku-u.ac.jp>
2024-10-25 11:13:41 +0900

    ブロック崩しゲームの作成
    ・ブロックの生成
    ・ボールとブロックの衝突検出
    ・スコアとライフの作成

--- ap0401.js
+++ ap0401.js
@@ -34,6 +34,10 @@ function init() {
   let score = 0;
   let life = 3;
   function setScore(score) {
+    document.getElementById("score").innerText
+      = String(Math.round(score)).padStart(8, "0");
+    document.getElementById("life").innerText
+      = (life > 0) ? "○○○".substring(0, life) : "-- Game Over --";
   }
 
   // Geometry の分割数
@@ -47,6 +51,7 @@ function init() {
     new THREE.SphereGeometry(ballR, nSeg, nSeg),
     new THREE.MeshPhongMaterial({ color: 0x808080, shininess: 100, specular: 0xa0a0a0 })
   );
+  ball.geometry.computeBoundingSphere();
   scene.add(ball);
 
   // ボールの移動
@@ -63,8 +68,6 @@ function init() {
       ball.position.x = paddle.position.x;
       ball.position.z = paddle.position.z - (paddleR + ballR);
     }
-    vBall.set(vx, 0, vz)
-    ball.position.addScaledVector(vBall, delta * speed);
   }
 
   // ボールの死活
@@ -75,12 +78,18 @@ function init() {
   function stopBall() {
     speed = 0;
     ballLive = false;
+    life = life-1;
   }
 
   // ボールを動かす
   function startBall() {
     ballLive = true;
     speed = 10;
+    if(life <= 0){
+      remakeBricks();
+      score = 0;
+      life = 3;
+    }
   }
 
   // マウスクリックでスタートする
@@ -228,6 +237,9 @@ function init() {
       else if ( ball.position.x < paddle.position.x - paddleL/2) {
         vx = -Math.abs(vx)
       }
+      if(nBrick <= 0) {
+        resetBrick();
+      }
     }
   
   }
@@ -235,7 +247,7 @@ function init() {
   // ブロック ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
   // ブロックの生成
   const bricks = new THREE.Group();
-  {
+  function makeBricks() {
     const color = ["white", "red", "yellow", "blue", "purple", "green"];
     const h = 0.8; /* ブロックの高さ */
     const d = 0.4; /* ブロックの奥行 */
@@ -245,23 +257,71 @@ function init() {
     // ブロックの幅
     const w = (hFrameW - 2 * vFrameW - (param.nCol + 1) * gapX) / param.nCol;
     // ブロックを並べる
+    for (let r = 0; r < param.nRow; r++) {
+      for (let c = 0; c < param.nCol; c++){
+        const brick = new THREE.Mesh(
+          new THREE.BoxGeometry(w,h,d),
+          new THREE.MeshLambertMaterial({color: color[r % color.length]})
+        );
+        brick.position.set(
+          (w + gapX) * ((param.nCol-1)/2 - c),
+          0,
+          -(d + gapZ) * r
+        )
+        brick.geometry.computeBoundingBox();
+        bricks.add(brick);
+        nBrick++;
+      }
+    }
 
     // ブロック全体を奥に移動する
+    bricks.position.z = -4;
+    scene.add(bricks);
 
+  }
+  makeBricks();
+
+  function remakeBricks() {
+    stopBall(); //ボールを止めて
+    scene.remove(bricks); //シーンからブロックを削除する
+    bricks.clear(); //ブロックのグループを空にして
+    nBrick = 0;
+    makeBricks();  //新しい数のブロックを作り
+    scene.add(bricks);  //シーンに追加する
   }
 
   // ブロックの衝突検出
   function brickCheck() {
     let hit = false;
-
+    const sphere = ball.geometry.boundingSphere.clone();
+    sphere.translate(ball.position);
     bricks.children.forEach((brick) => {
+      if (!hit && brick.visible) {
+        let box = brick.geometry.boundingBox.clone();
+        box.translate(bricks.position);
+        box.translate(brick.position);
+        if (box.intersectsSphere(sphere)){
+          hit = true;
+          brick.visible = false;
+          nBrick--;
+          score += (-brick.position.z + 1) * 100;
+          if(nBrick == 0){
+            score += 1000;
+          }
+          vz = -vz;
+        }
+      }
     });
   }
 
 
   // ブロックの再表示
   function resetBrick() {
-
+    nBrick = 0;
+    bricks.children.forEach((brick) => {
+      brick.visible = true;
+      nBrick++;
+    });
   }
 
   // 光源の設定
@@ -292,6 +352,7 @@ function init() {
     camera.updateProjectionMatrix();
     // 座標軸の表示
     axes.visible = param.axes;
+    axes.visible = false;
     // ゲーム画面の更新
     let delta = clock.getDelta(); // 経過時間の取得
     frameCheck(); // 枠の衝突判定
@@ -311,6 +372,8 @@ function init() {
   gui.add(param, "y", -40, 80);
   gui.add(param, "z", -40, 80);
   gui.add(param, "axes");
+  gui.add(param, "nRow" , 1, 10, 1).onChange(remakeBricks);
+  gui.add(param, "nCol" , 1, 10, 1).onChange(remakeBricks);
   gui.close();
   // 描画
   render();
